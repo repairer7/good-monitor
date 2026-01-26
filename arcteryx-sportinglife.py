1a78c868d442f3225b18244ea5e6e161fed9639b
repairer7 <51566775+repairer7@users.noreply.github.com>
2026-01-26 14:34:43 +0800

    Add HTTPAdapter and Retry imports for requests

--- arcteryx-sportinglife.py
+++ arcteryx-sportinglife.py
@@ -4,6 +4,8 @@ import os
 import json
 import logging
 import urllib.parse
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 # ========== 设置日志 ==========
 log_dir = os.path.join(os.getcwd(), "tmp/good-monitor")
